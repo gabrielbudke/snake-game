87a1c9742c76d4430674cc596bf0a08381f60f4a
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-05-13 14:17:32 +0000

    :sparkles: Desenvolvendo o backend do joguinho

--- 1st-proof-of-concept/server.js
+++ 1st-proof-of-concept/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import http from 'http';
+import createGame from './public/game.js';
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.static('public'));
+
+const game = createGame();
+
+console.log('> game.state', game.state)
 
 server.listen(3000, () => {
    console.log('> Server listening on port: 3000');
